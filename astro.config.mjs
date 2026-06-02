42d5278c124bbe4c01b9ff7ff65e24ebca25409f
giovanniribeiro-my-our-your-side <nucleojoresportivo@gmail.com>
2026-06-01 21:24:33 -0300

    ajustes de bugs 1 do 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import  from '@astrojs/';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://nucleo-ufsc.vercel.app',
-  integrations: [()],
+  integrations: [tailwind()],
 });
