66016d262625e330c4183c0b6ab0ad7855117dd5
nucleojoresportivo-giovanni <nucleojoresportivo@gmail.com>
2026-06-02 19:01:06 -0300

    adiciona keystatic cms v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://nucleo-ufsc.vercel.app',
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), keystatic()],
 });
--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,6 +0,0 @@
-import { makeRouteHandler } from '@keystatic/astro/api';
-import config from '../../../keystatic.config';
-
-export const prerender = false;
-
-export const all = makeRouteHandler({ config });
